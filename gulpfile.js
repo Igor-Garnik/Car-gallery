f3abd52e2b81b823c40fa5a71194bf6683f4883a
Igor-Garnik <garnik_igor@ukr.net>
2018-06-28 11:49:56 +0300

    fix problem with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ let rollup = require('gulp-better-rollup');
 let babel = require('rollup-plugin-babel');
 let sourcemaps = require('gulp-sourcemaps');
 let cssmin = require('gulp-cssmin');
-let del = require('del');
 let browserSync = require('browser-sync').create();
 
 
@@ -15,39 +14,39 @@ gulp.task('clean', function() {
 
 gulp.task('js', function() {
 	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-		.pipe(gulp.dest("src/build/tools"))
+		.pipe(gulp.dest("public/build/tools"))
 		.pipe(browserSync.stream());
 })
 
 gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
+    return gulp.src('public/css/*.css')
         .pipe(concat('main.min.css'))
         .pipe(sourcemaps.init())
         .pipe(cssmin())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/build/css'))
+        .pipe(gulp.dest('public/build/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
-    return gulp.src(['src/js/app.js'])
+    return gulp.src(['public/js/app.js'])
         .pipe(sourcemaps.init())
         .pipe(rollup({plugins: [babel()]}, 'iife'))
         .pipe(concat('app.min.js'))
         // .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/build/js'))
+        .pipe(gulp.dest('public/build/js'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
 	return gulp.src('node_modules/font-awesome/fonts/*')
-		.pipe(gulp.dest("src/build/fonts"));
+		.pipe(gulp.dest("public/build/fonts"));
 });
 
 gulp.task('fa', function() {
 	return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-		.pipe(gulp.dest("src/build/css"));
+		.pipe(gulp.dest("public/build/css"));
 });
 
 /* gulp.task('img', function () {
@@ -56,8 +55,8 @@ gulp.task('fa', function() {
 });  */
 
 gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles, ['css']);
+    gulp.watch('public/js/app.js', ['scripts']);
+    gulp.watch('public/css/*.css', ['css']);
 });
 
 gulp.task('default', ['watch', 'js', 'css', 'scripts', 'fonts', 'fa']);
---
+++ public/js/router.js
@@ -0,0 +1,50 @@
+import utils from './utils.js';
+
+import GalleryModel from './gallery/gallery.model.js';
+import GalleryView from './gallery/gallery.view.js';
+import GalleryController from './gallery/gallery.controller.js';
+
+import LoginModel from './login/login.model.js';
+import LoginView from './login/login.view.js';
+import LoginController from './login/login.controller.js';
+
+import ProfileModel from './profile/profile.model.js';
+import ProfileView from './profile/profile.view.js';
+import ProfileController from './profile/profile.controller.js'
+
+let routeConfig = {
+    "" : () => {
+        utils.initTemplate(wrapper, "login-view");
+        let model = new LoginModel();
+        let view = new LoginView();
+        let controller = new LoginController(model, view, utils);
+    }, 
+    "gallery" : () => {
+        utils.initTemplate(wrapper, "gallery-view");
+        let model = new GalleryModel;
+        let view = new GalleryView;
+        let observer = new Observer;
+        new GalleryController(model, view, observer, utils);
+    },
+    "profile" : () => {
+        utils.initTemplate(wrapper, "profile-view");
+        let model = new ProfileModel;
+        let view = new ProfileView;
+        new ProfileController(model, view, utils);
+    }
+}
+
+function activateRoute(routeName){
+    let route = routeConfig[routeName];
+    route && route();    
+}
+
+export function updateRoute() {
+    let routeName = document.location.hash.replace(/^#/, '');
+    if(routeName) {
+        utils.navigateTo(routeName); 
+        activateRoute(routeName);
+    } else {
+        activateRoute(routeName);
+    }
+}
