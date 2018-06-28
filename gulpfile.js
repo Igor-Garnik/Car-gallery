5d6ccfdf2a65deb17517f33e34b9d5f7504471f1
Igor-Garnik <garnik_igor@ukr.net>
2018-06-28 09:48:26 +0300

    edit package.json and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,55 +8,56 @@ let cssmin = require('gulp-cssmin');
 let del = require('del');
 let browserSync = require('browser-sync').create();
 
-let paths = {
-    vendor: ['src/js/tools/*.js'],
-    styles: ['src/css/*.css'],
-    fonts: ['src/fonts/*.*'],
-    img: ['src/img/*.*']
-};
 
 gulp.task('clean', function() {
     return del(['build']);
 });
 
+gulp.task('js', function() {
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+		.pipe(gulp.dest("src/build/tools"))
+		.pipe(browserSync.stream());
+})
+
 gulp.task('css', function() {
-    return gulp.src(paths.styles)
+    return gulp.src('src/css/*.css')
         .pipe(concat('main.min.css'))
         .pipe(sourcemaps.init())
         .pipe(cssmin())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/build/css'))
+        .pipe(gulp.dest('src/build/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('public/js/app.js')
+    return gulp.src(['src/js/app.js'])
         .pipe(sourcemaps.init())
         .pipe(rollup({plugins: [babel()]}, 'iife'))
         .pipe(concat('app.min.js'))
         // .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/build/js'))
+        .pipe(gulp.dest('src/build/js'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('vendor', function() {
-    return gulp.src(paths.vendor)
-        .pipe(gulp.dest('public/build/js/vendor'))
+gulp.task('fonts', function() {
+	return gulp.src('node_modules/font-awesome/fonts/*')
+		.pipe(gulp.dest("src/build/fonts"));
 });
 
-gulp.task('fonts', function () {
-    return gulp.src(paths.fonts)
-        .pipe(gulp.dest('public/build/fonts'))
+gulp.task('fa', function() {
+	return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+		.pipe(gulp.dest("src/build/css"));
 });
-gulp.task('img', function () {
+
+/* gulp.task('img', function () {
     return gulp.src(paths.img)
         .pipe(gulp.dest('public/build/img'))
-});
+});  */
 
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.styles, ['css']);
 });
 
-gulp.task('default', ['watch', 'css', 'scripts', 'vendor', 'fonts', 'img']);
+gulp.task('default', ['watch', 'js', 'css', 'scripts', 'fonts', 'fa']);
--- index.js
+++ index.js
@@ -18,18 +18,7 @@ server.post('/login', (req, res) => {
     }
 })
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-// server.use((req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now()
-//   }
-//   // Continue to JSON Server router
-//   next()
-// })
-
 // Use default router
-
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
